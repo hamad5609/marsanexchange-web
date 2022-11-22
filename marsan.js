a520ea2a217bfbca134954bd00f710252e12e3e7
Hamad <92583418+hamad5609@users.noreply.github.com>
2022-11-22 14:23:29 +0500

    Initial Commit

---
+++ marsan.js
@@ -0,0 +1,197 @@
+var marsan = {
+
+    "general_video_fr":"Vidéo",
+    "general_video_en":"Video",
+
+    "general_video_close_fr":"fermer",
+    "general_video_close_en":"close",
+
+    "general_copied_fr":"Contrat copié au presse-papier",
+    "general_copied_en":"Contract copied to clipboard",
+
+
+
+
+
+
+    "exchange_sub_fr":"Besoin d'acheter<br>ou vendre de<br>la crypto?",
+    "exchange_sub_en":"Need to<br>buy and sell<br>crypto?",
+    "exchange_p_fr":"<strong>Aucun problème!</strong> Nous offrons un service sécuritaire, privé et personnalisé d'échange via Telegram. <strong>Profitez du contact humain</strong>, ici même au Canada, pour tous vos besoins de Bitcoin et Ethereum.",
+    "exchange_p_en":"<strong>No problem!</strong> We offer a secure, private and personalized exchange service via Telegram. <strong>Get the human touch</strong>, right here in Canada, for all your Bitcoin and Ethereum needs.",
+    "exchange_button_fr":"Écrivez-nous sur Telegram",
+    "exchange_button_en":"Let's talk on Telegram",
+
+
+
+
+
+
+
+
+    "token_sub_fr":"Suralimentez votre expérience crypto",
+    "token_sub_en":"Supercharge<br>your crypto<br>experience",
+    "token_p_fr":"Obtenez des <strong>frais de transaction réduits</strong> dans notre prochaine app d'échange. Nous sommes continuellement en train de penser aux nombreuses façons dont nous pouvons servir la communauté crypto; <strong>tous alimenté par le Token MRS</strong>.",
+    "token_p_en":"Get <strong>lower transaction fees</strong> in our upcoming exchange app. We're continuously looking ahead at the many ways we can better serve the crypto community; <strong>all supercharged by the MRS Token</strong>.",
+    "token_contract_fr":"Contrat du Token",
+    "token_contract_en":"Token Contract",
+
+    "token_buy_title_fr":"Acheter le Token",
+    "token_buy_title_en":"Buy the Token",
+    "token_buy_p_fr":"Acheter des tokens MRS à travers Uniswap V2 en échangeant des ETH.",
+    "token_buy_p_en":"You can buy MRS tokens through Uniswap V2 by swapping ETH.",
+    "token_buy_button_fr":"Ouvrir Uniswap",
+    "token_buy_button_en":"Open Uniswap",
+
+    "token_market_title_fr":"Prix et capitalisation",
+    "token_market_title_en":"Price and market cap",
+    "token_market_p_fr":"Quel est le prix? Comment ça se vend? Montrez-moi la grosse montée et descente!",
+    "token_market_p_en":"What's the price? How is it trading? Show me the giant raise and dip!",
+    "token_market_button_fr":"Ouvrir CoinMarketCap",
+    "token_market_button_en":"Check on CoinMarketCap",
+
+    "token_whitepaper_title_fr":"Whitepaper",
+    "token_whitepaper_title_en":"Whitepaper",
+    "token_whitepaper_p_fr":"Découvrez le Token MRS! Apprenez les bases, objectifs et avantages.",
+    "token_whitepaper_p_en":"Get to know the MRS Token! Learn the basics, goals and incentives.",
+    "token_whitepaper_button_fr":"Lire le PDF",
+    "token_whitepaper_button_en":"Read the PDF",
+
+    "token_help_title_fr":"Besoin d'aide?",
+    "token_help_title_en":"Need help?",
+    "token_help_p_fr":"Voici un petit guide de démarage vidéo pour vous aider à acheter le Token.",
+    "token_help_p_en":"Here's a quick getting started video to help you buy the Token.",
+    "token_help_button_fr":"Disponible bientôt",
+    "token_help_button_en":"Available soon",
+
+    "tokenomics_fr":"Tokenomics",
+    "tokenomics_en":"Tokenomics",
+    "tokenomics_p_fr":"Pourquoi devrais-je faire confiance à ce Token? Comment a-t-il été créé?",
+    "tokenomics_p_en":"Why should I trust this Token? How was it created? What's the deal?",
+    "tokenomics_action_fr":"Spécifications",
+    "tokenomics_action_en":"Show the data",
+
+
+    "tokenomics_1_fr":"Aucun minage",
+    "tokenomics_1_en":"No mint function",
+    "tokenomics_1_sub_fr":"10M de tokens au total et rien de plus. Jamais.",
+    "tokenomics_1_sub_en":"10M total tokens and new tokens can't be created. Ever.",
+
+    "tokenomics_2_fr":"Aucune prévente",
+    "tokenomics_2_en":"No pre-sale",
+    "tokenomics_2_sub_fr":"Pour personne. Nous avons même acheté nos propres MRS d'Uniswap comme tout le monde.",
+    "tokenomics_2_sub_en":"For no one. We even bought our own MRS from Uniswap like everyone else. Fair and square!",
+
+    "tokenomics_3_fr":"Notre wallet bloqué pour 2 ans",
+    "tokenomics_3_en":"Our wallet locked for 2 years",
+    "tokenomics_3_sub_fr":"Nous avons acheté 20% de la liquidité initiale et l'avons bloquée pour 2 ans sur Team.Finance.",
+    "tokenomics_3_sub_en":"We bought 20% of the initial liquidity and locked it for 2 years on Team.Finance.",
+
+    "tokenomics_4_fr":"Liquidité initiale bloqué pour toujours",
+    "tokenomics_4_en":"Initial liquidity locked forever",
+    "tokenomics_4_sub_fr":"Nous avons créé le contrat et directement mis 100% des tokens sur Uniswap, bloqué à vie sur Team.Finance.",
+    "tokenomics_4_sub_en":"We created the contract, directly put all of it on Uniswap and locked it for life on Team.Finance.",
+
+    "contract_fr":"Contrat",
+    "contract_en":"Contract",
+    "contract_p_fr":"Avez-vous besoin du numéro de contrat du Token sur le réseau Ethereum?",
+    "contract_p_en":"Do you need the Token's contract number on the Ethereum network?",
+
+    "tokenomics_team_fr":"Valider sur <img src='img/team.png' src='Team.Finance'>",
+    "tokenomics_team_en":"Validate on <img src='img/team.png' src='Team.Finance'>",
+
+
+
+    "crypto_sub_fr":"Besoin de plus d'information sur la crypto?",
+    "crypto_sub_en":"Need more information about crypto?",
+    "crypto_p_fr":"Visitez la section sur la cryptomonnaie de l'Autorité des marchés financiers. L'AMF fournit de l'information à jour; du fonctionnement technique de base aux nombreux risques reliés à la crypto. Il est fortement recommandé au grand public de consulter et comprendre ces concepts.",
+    "crypto_p_en":"Visit the cryptocurrencies section of the Autorité des marchés financiers. The AMF provides up-to-date information; from the technical basics to the many risks associated with crypto. It is highly recommended for the general public to consult and understand these guidelines.",
+    "crypto_button_fr":"En apprendre plus",
+    "crypto_button_en":"Learn more",
+
+    "crypto_button_link_fr":"https://lautorite.qc.ca/grand-public/investissements/bitcoin-et-autres-cryptomonnaies",
+    "crypto_button_link_en":"https://lautorite.qc.ca/en/general-public/investments/bitcoin-and-other-virtual-currencies",
+
+
+
+
+
+
+    "comm_sub_fr":"Vraiment passionné de la crypto?",
+    "comm_sub_en":"Really passionate about crypto?",
+    "comm_p_fr":"Ça tombe bien! Rejoignez notre communauté sur Telegram, jetez un coup d'oeil à notre Medium ou suivez l'un de nos nombreux comptes sur les réseaux sociaux pour rester à jour.",
+    "comm_p_en":"Well you're in luck! Join our community on Telegram, take a look at our Medium or follow one of our many social media accounts to stay up-to-date.",
+
+    "comm_tg_title_fr":"Telegram",
+    "comm_tg_title_en":"Telegram",
+    "comm_tg_p_fr":"Suivez notre channel de nouvelles ou rejoignez notre groupe de discussion.",
+    "comm_tg_p_en":"Follow our news channel or join the conversation with other members.",
+    "comm_tg_button_news_fr":"Nouvelles",
+    "comm_tg_button_news_en":"News",
+    "comm_tg_button_comm_fr":"Communauté",
+    "comm_tg_button_comm_en":"Community",
+
+    "comm_md_title_fr":"Blog",
+    "comm_md_title_en":"Blog",
+    "comm_md_p_fr":"Où retrouver les dernières pensées et recommandations de l'équipe Marsan Exchange.",
+    "comm_md_p_en":"Get the latest thoughts and tips from the Marsan Exchange team.",
+    "comm_md_button_fr":"Lire sur Medium",
+    "comm_md_button_en":"Read on Medium",
+
+    "comm_sn_title_fr":"Réseaux sociaux",
+    "comm_sn_title_en":"Social networks",
+    "comm_sn_p_fr":"Préférez-vous avoir les dernières publications de Marsan Exchanges dans vos feeds?",
+    "comm_sn_p_en":"Do you prefer having the latest about Marsan Exchange on your feeds?",
+    "comm_sn_button_ig_fr":"Instagram",
+    "comm_sn_button_ig_en":"Instagram",
+    "comm_sn_button_tw_fr":"Twitter",
+    "comm_sn_button_tw_en":"Twitter",
+    "comm_sn_button_fb_fr":"Facebook",
+    "comm_sn_button_fb_en":"Facebook",
+    "comm_sn_button_li_fr":"Linkedin",
+    "comm_sn_button_li_en":"Linkedin",
+    "comm_sn_button_gi_fr":"Github",
+    "comm_sn_button_gi_en":"Github",
+
+
+
+
+    "footer_p_fr":"Notre mission est de donner les meilleurs services en crypto au Canada. Fait à Montréal, Québec.",
+    "footer_p_en":"Our mission is to offer the best crypto services to Canadians. Made in Montréal, Quebec.",
+
+    "footer_fintrac_fr":"Licence Fintrac: <a href='https://www10.fintrac-canafe.gc.ca/msb-esm/publique/informations-detaillees/renseignements-de-l-esm/7b226d73624f72674e756d626572223a3136313835332c227072696d617279536561726368223a7b226f72674e616d65223a224d415253414e222c2273656172636854797065223a317d7d/' target='_blank'>M20879761</a>",
+    "footer_fintrac_en":"Fintrac license: <a href='https://www10.fintrac-canafe.gc.ca/msb-esm/public/detailed-information/msb-details/7b226d73624f72674e756d626572223a3136313835332c227072696d617279536561726368223a7b226f72674e616d65223a224d415253414e222c2273656172636854797065223a317d7d/' target='_blank'>M20879761</a>",
+
+    "footer_exchange_fr":"Échange",
+    "footer_exchange_en":"Exchange",
+
+    "footer_con_fr":"Connecter",
+    "footer_con_en":"Connect",
+
+    "footer_com_fr":"À propos",
+    "footer_com_en":"About",
+
+    "footer_com_terms_fr":"Conditions d'utilisation",
+    "footer_com_terms_en":"Terms of use",
+
+    "footer_com_privacy_fr":"Politique de confidentialité",
+    "footer_com_privacy_en":"Privacy policy",
+
+    "footer_com_contact_fr":"Contact",
+    "footer_com_contact_en":"Contact",
+
+    "footer_com_cookiepref_fr":"Préférence relative aux cookies",
+    "footer_com_cookiepref_en":"Cookie preferences",
+    
+
+    "exchange_presskit_sub_en":"brand identity assets<br>to download.",
+    "exchange_presskit_sub_fr":"actifs d'identité de marque<br>Télécharger.",
+    "exchange_presskit_p_en":"Thank you for using these resources made especially for your use and making noise around all these beautiful projects. <strong>Feel free to download our logo, colors palette or team member photos.</strong> ",
+    "exchange_presskit_p_fr":"Merci d'utiliser ces ressources faites spécialement pour votre usage et de faire du bruit autour de tous ces beaux projets. <strong>N'hésitez pas à télécharger notre logo, notre palette de couleurs ou les photos des membres de l'équipe.</strong> ",
+   
+    "exchange_presskit_logos_en":"our<br>logos",
+    "exchange_presskit_logos_fr":"nos<br>logos",
+
+
+    "000000":"0000000"
+
+}
